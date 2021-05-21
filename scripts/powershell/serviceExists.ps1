fb9474711d10d2b723612b99230eb43ce2dff8db
Aaron Horeth <aaron.horeth.e@gmail.com>
2021-05-21 09:03:00 -0400

    Create serviceExists.ps1

---
+++ scripts/powershell/serviceExists.ps1
@@ -0,0 +1,14 @@
+Function ServiceExists {
+    Param([string]$serviceName)
+
+    If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+        If ((Get-Service $serviceName).Status -eq 'Running') {
+            Stop-Service $serviceName
+            Write-Host "Found Service:" $serviceName ", stopping..."
+            Return 1
+        }
+    } Else {
+        Write-Host "Service:" $serviceName " not found..."
+        Return 0
+    }
+}
