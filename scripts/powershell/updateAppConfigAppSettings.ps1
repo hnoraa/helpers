e8e5c606966c4e7d143e52e84f7b840c028cb4e1
Aaron Horeth <aaron.horeth.e@gmail.com>
2021-05-21 09:09:38 -0400

    Update updateAppConfigAppSettings.ps1

--- scripts/powershell/updateAppConfigAppSettings.ps1
+++ scripts/powershell/updateAppConfigAppSettings.ps1
@@ -1,8 +1,15 @@
 Function UpdateAppSetting {
-    Param([string]$config, [string]$name, [string]$value) 
+    Param([string]$config, [string]$name, [string]$value)
+    
     Write-Host "Updating $config setting $name to $value..."
+    
+    # load
     $doc = (Get-Content $config) -as [Xml]
+    
+    # load the root of the app config and replace the value for the given key
     $root = $doc.get_DocumentElement()
     $root.appSettings.add | ForEach-Object { If ($_.key -eq $name) { $_.value = $value } }
+    
+    # save
     $doc.Save($config)
 }
