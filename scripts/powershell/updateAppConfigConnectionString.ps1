549535b7c3a1df71278426e20fd019203d2f5c61
Aaron Horeth <aaron.horeth.e@gmail.com>
2021-05-21 09:11:16 -0400

    Update updateAppConfigConnectionString.ps1

--- scripts/powershell/updateAppConfigConnectionString.ps1
+++ scripts/powershell/updateAppConfigConnectionString.ps1
@@ -1,9 +1,22 @@
 Function UpdateConnectionString {
-    Param([string]$config, [string]$connectionString) 
+    Param(
+        # the config file
+        [string]$config, 
+        
+        # the new connection string
+        [string]$connectionString
+    )
+    
     Write-Host "Updating $config sql connection string..."
+    
+    # load
     $doc = (Get-Content $config) -as [Xml]
+    
+    # update the connection string
     $root = $doc.get_DocumentElement()
     $cns = $root.connectionStrings.SelectNodes("add")
     $cns.SetAttribute("connectionString", $connectionString)
+    
+    # save
     $doc.Save($config)
 }
