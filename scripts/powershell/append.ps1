2a41dbfcbac15e8b56a6a51a4af93b916c2b0957
Aaron Horeth <aaron.horeth.e@gmail.com>
2022-04-11 14:57:07 -0400

    Append and Overwrite
    Examples of appending and overwriting files

---
+++ scripts/powershell/append.ps1
@@ -0,0 +1,7 @@
+﻿# append to a file
+# $PSScriptRoot is cwd of this script
+$outputFile = $PSScriptRoot + "\test.txt"
+$today = [System.Math]::Round((date -UFormat %s),0)
+
+# write to the file
+"Write - $($today)" | Out-File -FilePath $outputFile -Append
---
+++ scripts/powershell/overwrite.ps1
@@ -0,0 +1,13 @@
+﻿# overwrite a file
+# example settings.ini file
+# [Header]
+# Author=Aaron
+# Description=Settings file
+# 
+# [Data]
+# XVal=0.00
+# YVal=0.00
+$testFile = $PSScriptRoot + "\settings.ini"
+
+(Get-Content $testFile) -Replace '^XVal=.*', "XVal=0.34" | Set-Content $testFile
+(Get-Content $testFile) -Replace '^YVal=.*', "YVal=7.60" | Set-Content $testFile
